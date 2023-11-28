9552e2278284a124115fb43e70c33def6b30cd72
tryiou <73646876+tryiou@users.noreply.github.com>
2023-11-28 14:16:01 +0100

    Update plugin-adapter.py

--- plugin-adapter.py
+++ plugin-adapter.py
@@ -32,7 +32,7 @@ logger = logging.getLogger(__name__)
 # namespace = 'cc-backend'
 
 coins = {}
-allowed = ["BLOCK", "BTC", "BCH", "LTC", "DASH", "DOGE", "DGB", "PIVX", "RVN", "SYS", "TZC", "XSN"]
+allowed = ["BLOCK", "BTC", "BCH", "LTC", "DASH", "DOGE", "DGB", "PIVX", "RVN", "SYS", "TZC", "XSN", "UNO"]
 hashx_cache = {}
 # try:
 #    config.load_incluster_config()
